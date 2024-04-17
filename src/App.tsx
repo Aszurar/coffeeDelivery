dc64505e017f4d9c49ade96c79c263b55cc411d9
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-04-17 15:36:28 -0300

    :tada: Iniciando projeto com Vite + chackra UI :tada:

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/Header'
+import { Flex } from '@chakra-ui/react'
+
+export function App() {
+  return (
+    <Flex w="100vw" h="100vh">
+      <Header />
+    </Flex>
+  )
+}
