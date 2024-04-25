e4dea0ece274c3530204d651ef139e54b53eee22
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-04-25 01:35:17 -0300

    ✨  Estruturando App✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@/components/Header'
 import { Flex } from '@chakra-ui/react'
+import { Banner } from './components/Banner'
 
 export function App() {
   return (
-    <Flex w="100vw" h="100vh">
+    <Flex w="100vw" h="100vh" flexDirection="column">
       <Header />
+      <Banner />
     </Flex>
   )
 }
