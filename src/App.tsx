dbf9f06bab03c3ad69def32ed19de7e04de80aea
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-05-05 00:45:34 -0300

    ✨Adição do Card de Café✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Header } from '@/components/Header'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react'
 import { Banner } from './components/Banner'
+import { CoffeeCard } from './components/CoffeeCard'
 
 export function App() {
   return (
-    <Flex w="100vw" h="100vh" flexDirection="column">
+    <Flex w="100vw" h="100vh" flexDir="column">
       <Header />
       <Banner />
+      <Flex
+        py="2rem"
+        mt="6.75rem"
+        gap="3.375rem"
+        flexDir="column"
+        w="100%"
+        mx="auto"
+        maxWidth="5xl.5"
+      >
+        <Heading fontSize="xx-large" fontWeight="800" color="gray.800">
+          Nossos cafés
+        </Heading>
+        <CoffeeCard />
+      </Flex>
     </Flex>
   )
 }
