9843bc4406034aeffb1a164dbdf8d057194550ec
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-05-17 12:17:44 -0300

    ✨ Adição do Header no Layout ✨

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,10 +1,14 @@
 import { Flex } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/Header'
+
 export default function DefaultLayout() {
   return (
     <Flex w="100vw" h="100vh" flexDir="column">
       <Flex w="100%" px="6" mx="auto" maxWidth="5xl.5" flexDir="column">
+        <Header />
+
         <Outlet />
       </Flex>
     </Flex>
