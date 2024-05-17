7bbfe95ab39a9537588bac5b0678a11f484c45b2
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-05-17 12:16:39 -0300

    :recycle: Refatoração do componente Counter para usar os m[etodos do Zustand :recycle:

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
 import { Box, Flex, IconButton, Text } from '@chakra-ui/react'
 import { Minus, Plus } from '@phosphor-icons/react'
 
-const COUNTER_INITIAL_VALUE = 1
+type CounterProps = {
+  h?: string
+  counter: number
+  onIncrement: () => void
+  onDecrement: () => void
+}
 
-export function Counter() {
-  const [counter, setCounter] = useState(COUNTER_INITIAL_VALUE)
-
-  function handleIncrement() {
-    setCounter(counter + 1)
-  }
-
-  function handleDecrement() {
-    if (counter > COUNTER_INITIAL_VALUE) {
-      setCounter(counter - 1)
-    }
-
-    if (counter === COUNTER_INITIAL_VALUE) {
-      // Remove item from cart
-    }
-  }
-
+export function Counter({
+  h = '2.375rem',
+  counter,
+  onDecrement,
+  onIncrement,
+}: CounterProps) {
   return (
-    <Flex gap="2" bg="gray.400" alignItems="center" rounded="md">
+    <Flex
+      gap="2"
+      bg="gray.400"
+      alignItems="center"
+      rounded="md"
+      w="fit-content"
+      h={h}
+    >
       <IconButton
         w="5"
         minW="5"
-        h="2.375rem"
+        h={h}
         borderLeftRadius="md"
         borderRightRadius={0}
         color="purple.500"
@@ -39,7 +39,7 @@ export function Counter() {
         }}
         aria-label="Remover 1 unidade do carrinho de compras"
         icon={<Minus width={14} height={14} />}
-        onClick={handleDecrement}
+        onClick={onDecrement}
       />
 
       <Box>
@@ -49,7 +49,7 @@ export function Counter() {
       <IconButton
         w="5"
         minW="5"
-        h="2.375rem"
+        h={h}
         bg="gray.400"
         color="purple.500"
         borderRightRadius="md"
@@ -62,7 +62,7 @@ export function Counter() {
         }}
         aria-label="Adicionar 1 unidade ao carrinho de compras"
         icon={<Plus width={14} height={14} />}
-        onClick={handleIncrement}
+        onClick={onIncrement}
       />
     </Flex>
   )
