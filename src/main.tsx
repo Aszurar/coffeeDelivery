189ca197f310b67add0dc1c934b3af61ec93093c
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-05-23 21:30:12 -0300

    💄 Configuração do visual do toast 💄

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+
 import { theme } from '@/styles/theme.ts'
+
+import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider
+      theme={theme}
+      toastOptions={{
+        defaultOptions: {
+          position: 'top-right',
+          isClosable: true,
+        },
+      }}
+    >
       <App />
     </ChakraProvider>
   </React.StrictMode>,
